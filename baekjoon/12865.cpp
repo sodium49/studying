f5ea9f0e4d1c888115b0d16b118487075e4a7863
sodium49 <sodiumna49@gmail.com>
2022-11-14 03:47:37 +0900

    12865 change

--- baekjoon/12865.cpp
+++ baekjoon/12865.cpp
@@ -7,22 +7,34 @@ int main(){
 	int dp[100001][100001] = {0};
 	int values[1001] = {0};
 	int weights[1001] = {0};
+	int i,j =0;
+	int tmpmax;
 
 	// Get number of items and knacksap maximum weights
-	cin >> N, K;
-	for(int i=1; i<=N ; i++){
+	cin >> N >> K;	// N for number of items, K for maximum weights
+	for(i=1; i<=N ; i++){
 		// scanf("%d %d", w[i], v[i]);
-		cin >> weights[i], values[i];
+		cin >> weights[i] >> values[i];
 	}
 
-	for (int j=1; j<=N; j++)
-		for (i=1; i<=K, i++){
-			//
+	for (i=1; i<=N; i++){
+		for (j=1; weights[i] >= j; j++){
+			dp[i][j] = dp[i-1][j];
 		}
-	// for (int=1;i<=K;i++){
-	// 	if(i == 1) dp[1][i] = 0;
-	
-	// }
+		for (;j<=K;j++){
+			for(int k=1;k<=j;k++){
+				tmpmax = (weights[j]>tmpmax) ? weights[j]: tmpmax;
+			}
+			dp[i][j] = (tmpmax>dp[i-1][j]) ? tmpmax:dp[i-1][j];
+			
+		}
+	}
 
+	for (i=1;i<=N;i++){
+		for(j=1;j<=K;j++){
+			cout << dp[i][j] << " ";
+		}
+		cout << endl;
+	}
 	return 0;
 }
