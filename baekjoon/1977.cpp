7a3919938a72d6e3cbe0af0ec8169b9ad7cae1c2
sodium49 <sodiumna49@gmail.com>
2025-12-26 13:15:23 +0900

    1977.cpp

---
+++ baekjoon/1977.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int M, N;
+    int sum = 0;
+    int min = 0;
+    cin >> M >> N;
+
+
+    int rootM = 1;
+    while (rootM * rootM < M) {
+        rootM++;
+    }
+    
+    int rootN = 1;
+    while (rootN * rootN < N) {
+        rootN++;
+    }
+    if(rootN *rootN > N){
+        rootN--;
+    }
+    // reutrn -1 in case no available between M and N
+    if (rootN < rootM){
+        cout << -1 << endl;
+        return 0;
+    }
+    
+    //get sum of square numbers between M and N
+    int cur = rootM;
+    while (cur <= rootN){
+        sum += cur*cur;
+        cur +=1;
+    }
+    cout << sum << endl;
+    cout << rootM*rootM << endl;
+
+    return 0;
+}
